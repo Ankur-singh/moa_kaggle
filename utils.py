b805de56ab2b55818f4a08e91d5bc2de6f23bc7c
Ankur Singh <as.ankursingh3.1@gmail.com>
2020-11-08 05:56:24 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,9 @@ def seed_everything(seed=42):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     
 def evaluate(y_true, y_pred):
     assert y_true.shape == y_pred.shape
